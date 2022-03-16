6a3deaa5ef8c936ec0eb81474188a1ace5729fe5
LeoEmVee <santito79@hotmail.com>
2022-03-16 17:12:33 +0100

    Refactor: Change Chlorinar font import method (from CDN to font-face)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import './fonts/Chlorinar.ttf';
 import App from './App';
 
 ReactDOM.render(
